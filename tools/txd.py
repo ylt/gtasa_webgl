80863f1b62c84c27c589c4e8e208d425dd961538
Joe Carter <theupperquartile@gmail.com>
2014-07-29 04:48:50 +0100

    Implemented PIL image getter

--- tools/txd.py
+++ tools/txd.py
@@ -1,4 +1,8 @@
+import ctypes
 import struct
+import s3tc
+from PIL import Image
+
 from enum import Enum
 
 class section():
@@ -9,7 +13,7 @@ class section():
     def readSection(self):
         section, section_size, rw_version = self.read_struct("<III")
         end = self.f.tell() + section_size
-        print("Section %s" % hex(section))
+        #print("Section %s" % hex(section))
         if section == 0x01: #struct
             return self.readStruct()
         elif section == 0x15:
@@ -47,6 +51,7 @@ class file(section):
         self.texture_count, self.unknown = self.read_struct("HH")
 
 class texture_native(section):
+    Compression = Enum('DXT1','DXT2')
     def __init__(self, f, end):
         self.f = f
         self.end = end
@@ -61,8 +66,8 @@ class texture_native(section):
         self.texture_wrap_u, \
         self.texture_name, \
         self.alpha_name, \
-        self.alpha_flags, \
-        self.direct3d_texture_format, \
+        self.raster_format, \
+        self.fourcc, \
         self.width,\
         self.height,\
         self.depth, \
@@ -75,15 +80,86 @@ class texture_native(section):
         self.texture = self.f.read(self.data_size)
         
         #convert back to ascii string and strip off the null terminators
-        self.texture_name = self.texture_name.decode('ascii').rstrip("\x00")
-        self.alpha_name = self.alpha_name.decode('ascii').rstrip("\x00")
+        self.texture_name = ctypes.c_char_p(self.texture_name).value.decode('ascii')
+        self.alpha_name = ctypes.c_char_p(self.alpha_name).value.decode('ascii')
         
     def hasAlpha(self):
-        format = self.direct3d_texture_format
-        if format == 0 or format == 0x16 or format == 0x31545844: # 1TXD
+        format = self.fourcc
+        compression = self.flags
+        if format == 0 or format == 0x16 or compression == 8:
             return False
-        elif format == 1 or format == 0x15 or format == 0x33545844: #3TXD
+        elif format == 1 or format == 0x15 or compression == 8:
             return True
         return None
     
-f = file("../fronten1.txd")
+    def getCompression(self):    
+        fourcc = self.fourcc
+        compression = self.flags
+        
+        if fourcc == 0x31545844 or fourcc == 1:
+            return "DXT1"
+        elif fourcc == 0x33545844 or fourcc == 3:
+            return "DXT3"
+        
+        if compression == 0 or compression == 1: #no compression
+            return False
+        
+        return False
+        
+    def getImage(self):
+        compression = self.getCompression()
+        if compression == "DXT1":
+            t = s3tc.decode_dxt1_rgba(self.texture, self.width, self.height)
+            return Image.frombuffer("RGBA", (self.width, self.height), t, 'raw', 'RGBA', 0, 1)
+        elif compression == "DXT3":
+            t = s3tc.decode_dxt3(self.texture, self.width, self.height)
+            return Image.frombuffer("RGBA", (self.width, self.height), t, 'raw', 'RGBA', 0, 1)
+        
+        raster_format = self.raster_format & 0x0100
+        if raster_format == 0x0500: #RGBA :D
+            return Image.frombuffer("RGBA", (self.width, self.height), self.texture, 'raw', 'RGBA', 0, 1)
+        elif raster_format == 0x0600: #RGB :D
+            return Image.frombuffer("RGB", (self.width, self.height), self.texture, 'raw', 'RGB', 0, 1)
+        elif raster_format == 0x0400:
+            return Image.frombuffer("L", (self.width, self.height), self.texture, 'raw', 'L', 0, 1)
+            
+        out = (ctypes.c_ubyte * (self.width * self.height * 4))()
+        out_pos = 0
+        for pos in range(0, self.width*self.height, self.depth/4):
+            r,g,b,a = 0,0,0,0
+            value, = struct.unpack_from("H", self.texture, pos)
+            if raster_format == 0x0000: # Default - Wut do here?
+                pass
+            elif raster_format == 0x0100: # FORMAT_1555
+                #(1 bit alpha, RGB 5 bits each; also used for DXT1 with alpha)
+                r = (value & 0xf8)        / 0xf8 * 255
+                g = ((value >> 5) & 0xf8) / 0xf8 * 255
+                b = ((value >> 10) & 0xf8)/ 0xf8 * 255
+                a = ((value >> 15) & 0x01)       * 255
+                
+            elif raster_format == 0x0200: # FORMAT_565
+                #(5 bits red, 6 bits green, 5 bits blue; also used for DXT1 without alpha)
+                r = (value & 0xf8)        / 0xf8 * 255
+                g = ((value >> 5) & 0xfc) / 0xfc * 255
+                b = ((value >> 11) & 0xf8)/ 0xf8 * 255
+                
+            elif raster_format == 0x0300: # FORMAT_4444
+                #(RGBA 4 bits each; also used for DXT3)
+                r = (value & 0xf0)        / 0xf0 * 255
+                g = ((value >> 4) & 0xf0) / 0xf0 * 255
+                b = ((value >> 8) & 0xf0) / 0xf0 * 255
+                a = ((value >> 12) & 0xf0)/ 0xf0 * 255
+                
+            elif raster_format == 0x0A00: # FORMAT_555
+                #(5 bits red, 6 bits green, 5 bits blue; also used for DXT1 without alpha)
+                r = (value & 0xf8)        / 0xf8 * 255
+                g = ((value >> 5) & 0xf8) / 0xfc * 255
+                b = ((value >> 10) & 0xf8)/ 0xf8 * 255
+            
+            out[out_pos] = b
+            out[out_pos+1] = g
+            out[out_pos+3] = r
+            out[out_pos+4] = a
+            out_pos += 4
+        return Image.frombuffer("RGBA", (self.width, self.height), out, 'raw', 'RGBA', 0, 1)
+f = file("../particle.txd")
