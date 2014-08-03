4a803003d25e38e0e18c6b69d0cfc70f459b84cf
Joe Carter <theupperquartile@gmail.com>
2014-08-03 13:30:00 +0100

    Make IPL reader class based

--- tools/readers/ipl.py
+++ tools/readers/ipl.py
@@ -1,112 +1,64 @@
-def convert(filename):
-    f = open(filename)
-    
-    data = {}
-    current = None     
-    
-    for line in f:
-        line = line.strip()
-    
-        if line[0] == "#":
-            continue
-        if current == None:
-            current = line
-            if current not in data:
-                data[current] = []
+class ipl():
+    def __init__(self, gtapath, filename):
+        self.inst = []
+        self.cull = []
+        self.path = []
+        self.grge = []
+        self.enex = []
+        self.pick = []
+        self.jump = []
+        self.tcyc = []
+        self.auzo = []
+        self.mult = []
+        self.cars = []
+        self.occl = []
+        self.zone = []
+        
+        self.gtapath = gtapath        
+        
+        self.read_text(filename)
+    def read_text(self, filename):
+        f = self.gtapath.getFileHandle(filename, "r")
+        
+        current = None     
+        
+        for line in f:
+            line = line.strip()
+        
+            if line[0] == "#":
                 continue
-        elif line == "end":
-            current = None
-            continue
-        
-        ldata = [item.strip() for item in line.split(",")]        
-             
-        
-        if current == "inst":
-            ID, ModelName, Interior, PosX, PosY, PosZ, RotX, RotY, RotZ, RotW, LOD = ldata
+            if current == None:
+                current = line
+            elif line == "end":
+                current = None
+                continue
             
-            entry = {
-                "ID":int(ldata[0]),
-                "ModelName":ldata[1],
-                "Interior":int(ldata[2]),
-                "PosX":float(ldata[3]),
-                "PosY":float(ldata[4]),
-                "PosZ":float(ldata[5]),
-                "RotX":float(ldata[6]),
-                "RotY":float(ldata[7]),
-                "RotZ":float(ldata[8]),
-                "RotW":float(ldata[9]),
-                "LOD":int(ldata[10]),
-            }                
-            data[current].append(entry)
-        elif current == "objs":
-            entry = {
-                "ID":int(ldata[0]),
-                "ModelName": ldata[1],
-                "TextureName": ldata[2],
-            }
-            dlen= len(ldata)
-            if dlen == 4: #Type 0
-                entry["ObjectCount"] = int(ldata[3])
-            elif dlen == 6: #Type 1
-                entry["ObjectCount"] = int(ldata[3])
-                entry["DrawDistance"] = float(ldata[4])
-                entry["Flags"] = int(ldata[5])
-            elif dlen == 7: #Type 2
-                entry["ObjectCount"] = int(ldata[3])
-                entry["DrawDistance"] = float(ldata[4])
-                entry["DrawDistance2"] = float(ldata[5])
-                entry["Flags"] = ldata[6]
-            elif dlen == 8: #Type 3
-                entry["ObjectCount"] = int(ldata[3])
-                entry["DrawDistance"] = float(ldata[4])
-                entry["DrawDistance2"] = float(ldata[5])
-                entry["DrawDistance3"] = float(ldata[6])
-                entry["Flags"] = ldata[7]
-            elif dlen == 5:
-                entry["DrawDistance"] = float(ldata[3])
-                entry["Flags"] = int(ldata[4])
+            ldata = [item.strip() for item in line.split(",")]        
+                 
+            
+            if current == "inst":
+                ID, ModelName, Interior, PosX, PosY, PosZ, RotX, RotY, RotZ, RotW, LOD = ldata
                 
-            entry["isLOD"] = False
-            
-            data[current].append(entry)
-        elif current == "tobj":
-            entry = {
-                "ID":int(ldata[0]),
-                "ModelName": ldata[1],
-                "TextureName": ldata[2],
-                "DrawDistance": float(ldata[4]),
-                "Flags":int(ldata[-3]),
-                "TImeOn":int(ldata[-2]),
-                "TImeOff":int(ldata[-1])
-            }
-            if len(entry) > 7:
-                entry["ObjectCount"] = int(ldata[3])
-            
-            
-            data[current].append(entry)
-        elif current == "anim":
-            data[current].append({
-                "ID":int(ldata[0]),
-                "ModelName": ldata[1],
-                "TextureName": ldata[2],
-                "AnimationName": ldata[3],
-                "DrawDistance": float(ldata[4]),
-                "Flags":int(ldata[5]),
-            })
-            
-            data[current].append(entry)
-        else:
-            data[current].append(ldata)
-
-    for k in list(data.keys()):
-        if len(data[k]) == 0:
-            del data[k]
-            
-    if "inst" in data:
-        for inst in data["inst"]:
+                entry = {
+                    "ID":int(ldata[0]),
+                    "ModelName":ldata[1],
+                    "Interior":int(ldata[2]),
+                    "PosX":float(ldata[3]),
+                    "PosY":float(ldata[4]),
+                    "PosZ":float(ldata[5]),
+                    "RotX":float(ldata[6]),
+                    "RotY":float(ldata[7]),
+                    "RotZ":float(ldata[8]),
+                    "RotW":float(ldata[9]),
+                    "LOD":int(ldata[10]),
+                }                
+                self.inst.append(entry)
+            else:
+                getattr(self, current, ldata)
+        
+    def markLOD(self):
+        for inst in self.inst:
             if inst["LOD"] == -1:
                 continue
             
-            data["inst"][inst["LOD"]]["isLOD"] = True
-        
-    return data
+            self.inst[inst["LOD"]]["isLOD"] = True
